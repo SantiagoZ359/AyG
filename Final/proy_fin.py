69e085afe3fc0db9f1550a5004bab1b505e89b96
SantiagoZ359 <sn.zapata@alumno.um.edu.ar>
2022-06-02 19:44:37 -0300

    inicios sesion ended

--- Final/proy_fin.py
+++ Final/proy_fin.py
@@ -1,3 +1,4 @@
+from httplib2 import ProxiesUnavailableError
 import pandas as pd
 import numpy as np
 import time 
@@ -11,8 +12,8 @@ def main():
     print('''
     OPCIONES:
     a) Lista de sesiones de usuario por ID
-    b) Direccion URL
-    c) Direccion IPV4
+    b) Inicio de sesion de un user en una determinada fecha
+    c) Tiempo total de la sesion de un usuario
     d) Contraseña segura
     e) Salir
     ''')
@@ -20,13 +21,13 @@ def main():
     print("\n")
     if input_1 == 'a':
         Id_ses()
-    #elif input_1 == 'b':
-        #use_url()
+    elif input_1 == 'b':
+        Inic_ses()
     #elif input_1 == 'c':
         #use_ipv4()
     #elif input_1 == 'd':
         #use_password()
-    elif input_1 == 'e':
+    elif input_1 == 'x':
         print("Saliendo del sistema...")
         exit()
     else:
@@ -37,25 +38,85 @@ def main():
 
 #Filtro por la categoria usuario
 def Id_ses():
+    
     #nombre del usuario
     nomb_usuario = input("Ingrese el nombre de usuario a analizar: ")
+    
     #corroboro que exista el nombre
     if nomb_usuario in df.values:
         #separo las categorias en ID y Usuario
+        #loc "elimina" categorias
         sf2 = df.loc[: , ["ID Conexion unico","Usuario"]]
+        #isin busca dentro de la categoria
         #filtro las categorias por el nombre de usuario
         sf3 = sf2[sf2["Usuario"].isin([nomb_usuario])]
         #exporto como csv
         sf3.to_csv("sesiones.csv")
+        
         print("Exito, archivo 'sessiones.csv' fue creado con exito")
         time.sleep(1)
+        
         print("regresando al menu")
+        
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print("..")
+        time.sleep(1)
+        print("...")
         time.sleep(1)
         main()
+    
     else:
         print("Nombre incorrecto, regresando al menu...")
         time.sleep(1)
         main()
+
+def Inic_ses():
+    nomb_usuario = input("Ingrese el nombre del usuario a analizar: ")
+    print("A continuacion, ingrese el rango de fecha")
+    print("El formato debe ser MM/DD/AAAA HH:MM (mes/dia/año hora:minuto)")
+    time.sleep(2)
+    #solicito la primer fecha
+    fech1 = input("ingrese la fecha inicial: ")
+    #solicito la segunda fecha
+    fech2 = input("ingrese la fecha final: ")
+    #verifico la existencia de los datos
+    if fech1 in df.values and fech2 in df.values and nomb_usuario in df.values and fech1 < fech2:
+        #"elimino" las categorias innecesarias
+        sf2 = df.loc[: , ["Usuario","Inicio de Conexi¢n"]]
+        #busco el nombre del usuario
+        sf3 = sf2[sf2["Usuario"].isin([nomb_usuario])]
+        #busco entre las fechas especificas
+        sf4 = sf3.loc[df["Inicio de Conexi¢n"].between(fech1, fech2)]
+        #exporto csv
+        sf4.to_csv("inicios_sesion.csv")
+        
+        print("proceso realizado con exito, verificar archivo 'inicios_sesion.csv")
+        print("Regresando al menu...")
+        time.sleep(1)
+        print(".")
+        time.sleep(1)
+        print("..")
+        time.sleep(1)
+        print("...")
+        time.sleep(1)
+        main()
+    else:
+        print("error dato incorrecto")
+        print("Volviendo al menu")
+        time.sleep(1)
+        main()
+
+
+
+
+#csegeview
+#8/28/2019 10:06
+#9/13/2019 11:39
+
+
+
 
 if __name__ == '__main__':
     main()
